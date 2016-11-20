f7a5595cc941a0343b9b6d1bf67f90546a9fd03d
Joe Innes <joe@joeinn.es>
2016-11-20 17:59:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const lon = 19.0402
 
 const map = L.map('mapcanvas').setView([lat, lon], 15)
 
-const CartoDB_Positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
   subdomains: 'abcd',
   maxZoom: 19
@@ -12,7 +12,7 @@ const CartoDB_Positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all
 let baseUrl = 'http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/bicycle-rental.json'
 let httpsProxy = 'https://crossorigin.me/'
 
-fetch(httpsProxy + baseUrl)
+window.fetch(httpsProxy + baseUrl)
   .then((data) => data.json())
   .then((data) => {
     const locs = data.data.list
@@ -31,7 +31,7 @@ fetch(httpsProxy + baseUrl)
         markerColor: colour
       })
       let marker = L.marker([location.lat, location.lon], {icon: bikeMarker}).addTo(map)
-      marker.bindPopup('<h1>' + location.name + '</h1><div style="float: right" id="directions"><i class="fa fa-location-arrow" aria-hidden="true"></i></div><strong>Available:</strong> ' + location.bikes + '<br /><strong>Capacity:</strong> ' + location.spaces)
+      marker.bindPopup('<h1>' + location.name + '</h1><div style="float: right" id="directions"><a href="https://maps.google.com/maps?saddr=here&daddr=' + location.lat + ',' + location.lon + '&dirflg=w" target="_blank"><i class="fa fa-location-arrow" aria-hidden="true"></i></a></div><strong>Available:</strong> ' + location.bikes + '<br /><strong>Capacity:</strong> ' + location.spaces)
     })
   })
   .catch((err) => {
